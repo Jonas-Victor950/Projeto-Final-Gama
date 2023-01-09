620c84037a15dd951e12b854e54706b416cdb82e
MarcosAurelio <aurelioleonel@gmail.com>
2023-01-08 22:43:46 -0300

    Adicionando rota agendadodia em agenda.routes.ts para os profissionais

--- backend/build/repositories/AgendaRepository.js
+++ backend/build/repositories/AgendaRepository.js
@@ -28,44 +28,88 @@ var AgendaRepository = /** @class */ (function () {
     AgendaRepository.prototype.agendaProfissionais = function () {
         return Agenda_1.Agenda.aggregate([
             {
+                $project: {
+                    _id: Number(0),
+                    A: '$$ROOT',
+                },
+            },
+            {
                 $lookup: {
+                    localField: 'A.profissionalServico',
                     from: 'ProfissionalServico',
-                    localField: 'profissionalServico',
                     foreignField: '_id',
-                    as: 'Profissionais_Servico',
+                    as: 'Ps',
                 },
             },
             {
-                $unwind: '$Profissionais_Servico',
-            },
-            {
-                $lookup: {
-                    from: 'Servico',
-                    localField: 'Profissionais_Servico.servico',
-                    foreignField: '_id',
-                    as: 'Servicos',
+                $unwind: {
+                    path: '$Ps',
+                    preserveNullAndEmptyArrays: false,
                 },
             },
             {
-                $unwind: '$Servicos',
-            },
-            {
                 $lookup: {
+                    localField: 'A.cliente',
                     from: 'Cliente',
-                    localField: 'cliente',
                     foreignField: '_id',
-                    as: 'Clientes',
+                    as: 'C',
                 },
             },
             {
-                $unwind: '$Clientes',
+                $unwind: {
+                    path: '$C',
+                    preserveNullAndEmptyArrays: false,
+                },
             },
             {
                 $lookup: {
-                    from: 'Profissional',
-                    localField: 'Profissionais_Servico.profissional',
+                    localField: 'Ps.servico',
+                    from: 'Servico',
                     foreignField: '_id',
-                    as: 'Profissionais',
+                    as: 'S',
+                },
+            },
+            {
+                $unwind: {
+                    path: '$S',
+                    preserveNullAndEmptyArrays: false,
+                },
+            },
+            {
+                $lookup: {
+                    localField: 'Ps.profissional',
+                    from: 'Profissional',
+                    foreignField: '_id',
+                    as: 'P',
+                },
+            },
+            {
+                $unwind: {
+                    path: '$P',
+                    preserveNullAndEmptyArrays: false,
+                },
+            },
+            {
+                $project: {
+                    'A._id': '$A._id',
+                    'A.data': '$A.data',
+                    'A.profissional': '$A.profissional',
+                    'A.cliente': '$A.cliente',
+                    'Ps.profissional': '$Ps.profissional',
+                    'Ps.servico': '$Ps.servico',
+                    'C._id': '$C._id',
+                    'C.nome': '$C.nome',
+                    'C.email': '$C.email',
+                    'C.telefone': '$C.telefone',
+                    'C.aniversaro': '$C.aniversaro',
+                    'C.sexo': '$C.sexo',
+                    'S._id': '$S._id',
+                    'S.servico': '$S.servico',
+                    'P._id': '$P._id',
+                    'P.nome': '$P.nome',
+                    'P.email': '$P.email',
+                    'P.telefone': '$P.telefone',
+                    _id: Number(0),
                 },
             },
         ]);
@@ -73,3 +117,49 @@ var AgendaRepository = /** @class */ (function () {
     return AgendaRepository;
 }());
 exports.default = new AgendaRepository();
+// agendaProfissionais() {
+//     return Agenda.aggregate([
+//       {
+//         $lookup: {
+//           from: 'ProfissionalServico',
+//           localField: 'profissionalServico',
+//           foreignField: '_id',
+//           as: 'Profissionais_Servico',
+//         },
+//       },
+//       {
+//         $unwind: '$Profissionais_Servico',
+//       },
+//       {
+//         $lookup: {
+//           from: 'Servico',
+//           localField: 'Profissionais_Servico.servico',
+//           foreignField: '_id',
+//           as: 'Servicos',
+//         },
+//       },
+//       {
+//         $unwind: '$Servicos',
+//       },
+//       {
+//         $lookup: {
+//           from: 'Cliente',
+//           localField: 'cliente',
+//           foreignField: '_id',
+//           as: 'Clientes',
+//         },
+//       },
+//       {
+//         $unwind: '$Clientes',
+//       },
+//       {
+//         $lookup: {
+//           from: 'Profissional',
+//           localField: 'Profissionais_Servico.profissional',
+//           foreignField: '_id',
+//           as: 'Profissionais',
+//         },
+//       },
+//     ]);
+//   }
+// }
--- backend/build/routes/agenda.routes.js
+++ backend/build/routes/agenda.routes.js
@@ -8,10 +8,10 @@ var AgendaController_1 = __importDefault(require("../controllers/AgendaControlle
 var auth_1 = __importDefault(require("../middlewares/auth")); // rota com senha token
 var routerAgenda = express_1.default.Router();
 routerAgenda.post('/agenda', auth_1.default, AgendaController_1.default.cadastroAgenda); // rota com senha token
-routerAgenda.get('/agenda', AgendaController_1.default.allAgenda);
-routerAgenda.get('/agenda/:id', AgendaController_1.default.allAgendaId);
-routerAgenda.put('/agenda/:id', AgendaController_1.default.agendaAtualizada);
-routerAgenda.delete('/agenda/:id', AgendaController_1.default.excluirAgenda);
+routerAgenda.get('/agenda', auth_1.default, AgendaController_1.default.allAgenda);
+routerAgenda.get('/agenda/:id', auth_1.default, AgendaController_1.default.allAgendaId);
+routerAgenda.put('/agenda/:id', auth_1.default, AgendaController_1.default.agendaAtualizada);
+routerAgenda.delete('/agenda/:id', auth_1.default, AgendaController_1.default.excluirAgenda);
 //Rota para agenda profisionais_serviços
 routerAgenda.get('/agendadodia', AgendaController_1.default.agendaProfissionais);
 exports.default = routerAgenda;
