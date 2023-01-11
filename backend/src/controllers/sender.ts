aff9fd1013e77a2111e792c0febba511c417054e
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2023-01-11 20:30:09 -0300

    taking off zap zap

--- backend/src/controllers/sender.ts
+++ backend/src/controllers/sender.ts
@@ -1,73 +1,73 @@
-import parsePhoneNumber, { isValidPhoneNumber } from "libphonenumber-js";
-import { create, Whatsapp, Message, SocketState } from "venom-bot";
+// import parsePhoneNumber, { isValidPhoneNumber } from "libphonenumber-js";
+// import { create, Whatsapp, Message, SocketState } from "venom-bot";
 
-export type QRCode = {
-  base64Qr: string;
-  asciiQR?: string;
-  attempts?: number;
-  urlCode?: string;
-};
+// export type QRCode = {
+//   base64Qr: string;
+//   asciiQR?: string;
+//   attempts?: number;
+//   urlCode?: string;
+// };
 
-class Sender {
-  private client: Whatsapp;
-  private connected: boolean;
-  private qr: QRCode;
-  static sendText: any;
+// class Sender {
+//   private client: Whatsapp;
+//   private connected: boolean;
+//   private qr: QRCode;
+//   static sendText: any;
 
-  get isConnected(): boolean {
-    return this.connected;
-  }
+//   get isConnected(): boolean {
+//     return this.connected;
+//   }
 
-  get qrCode(): QRCode {
-    return this.qr;
-  }
+//   get qrCode(): QRCode {
+//     return this.qr;
+//   }
 
-  constructor() {
-    this.initialize();
-  }
+//   constructor() {
+//     this.initialize();
+//   }
 
-  async sendText(to: string, body: string) {
-    if (!isValidPhoneNumber(to, "BR")) {
-      throw new Error("This number is not valid");
-    }
+//   async sendText(to: string, body: string) {
+//     if (!isValidPhoneNumber(to, "BR")) {
+//       throw new Error("This number is not valid");
+//     }
 
-    let phoneNumber = parsePhoneNumber(to, "BR")
-      ?.format("E.164")
-      ?.replace("+", "") as string;
+//     let phoneNumber = parsePhoneNumber(to, "BR")
+//       ?.format("E.164")
+//       ?.replace("+", "") as string;
 
-    phoneNumber = phoneNumber.includes("@c.us")
-      ? phoneNumber
-      : `${phoneNumber}@c.us`;
+//     phoneNumber = phoneNumber.includes("@c.us")
+//       ? phoneNumber
+//       : `${phoneNumber}@c.us`;
 
-    await this.client.sendText(phoneNumber, body);
-  }
+//     await this.client.sendText(phoneNumber, body);
+//   }
 
-  private initialize() {
-    const qr = (base64Qr: string) => {
-      this.qr = { base64Qr };
-    };
+//   private initialize() {
+//     const qr = (base64Qr: string) => {
+//       this.qr = { base64Qr };
+//     };
 
-    const status = (statusSession: string) => {
-      this.connected = [
-        "Connected",
-        "isLogged",
-        "qrReadSuccess",
-        "chatsAvailable",
-      ].includes(statusSession);
-    };
+//     const status = (statusSession: string) => {
+//       this.connected = [
+//         "Connected",
+//         "isLogged",
+//         "qrReadSuccess",
+//         "chatsAvailable",
+//       ].includes(statusSession);
+//     };
 
-    const start = (client: Whatsapp) => {
-      this.client = client;
+//     const start = (client: Whatsapp) => {
+//       this.client = client;
 
-      client.onStateChange((state: any) => {
-        this.connected = state === SocketState.CONNECTED;
-      });
-    };
+//       client.onStateChange((state: any) => {
+//         this.connected = state === SocketState.CONNECTED;
+//       });
+//     };
 
-    create("ws-sender-dev")
-      .then((client: any) => start(client))
-      .catch((error: any) => console.log(error));
-  }
-}
+//     create("ws-sender-dev")
+//       .then((client: any) => start(client))
+//       .catch((error: any) => console.log(error));
+//   }
+// }
 
-export default Sender;
+// export default Sender;
