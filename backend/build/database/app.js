daf275ed2f5e39023a09072e0c0277e6519cdc4f
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2023-01-13 13:55:05 -0300

    MongoDB conection

--- backend/build/database/app.js
+++ backend/build/database/app.js
@@ -5,12 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var Conection_1 = __importDefault(require("./Conection"));
 var mongoose_1 = __importDefault(require("mongoose"));
+var default_1 = __importDefault(require("./default"));
 mongoose_1.default.set('strictQuery', true);
-/*mongoose.connect(
- `mongodb+srv://${authDB.user}:${authDB.pass}@cluster0.ve0zm2e.mongodb.net/?retryWrites=true&w=majority`
-);
-const mongoDB = new Conection(
-  `mongodb+srv://${authDB.user}:${authDB.pass}@cluster0.ve0zm2e.mongodb.net/?retryWrites=true&w=majority`
-);*/
-var mongoDB = new Conection_1.default('mongodb://127.0.0.1:27017/beleza_na_agenda');
+mongoose_1.default.connect("mongodb+srv://".concat(default_1.default.user, ":").concat(default_1.default.pass, "@cluster0.ve0zm2e.mongodb.net/?retryWrites=true&w=majority"));
+var mongoDB = new Conection_1.default("mongodb+srv://".concat(default_1.default.user, ":").concat(default_1.default.pass, "@cluster0.ve0zm2e.mongodb.net/?retryWrites=true&w=majority"));
+// const mongoDB = new Conection('mongodb://127.0.0.1:27017/beleza_na_agenda');
 exports.default = mongoDB;
