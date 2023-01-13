69c435a0ed5ba3dac3943eb22531d6f4a73fa1de
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2023-01-12 21:24:46 -0300

    Removing where from authcontroller

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -6,42 +6,44 @@ import secret from "../configs/secret";
 
 const AuthController = {
   async loginCliente(req: Request, res: Response) {
-  
-    const { email, senha } = req.body;
-
-    const cliente = await Cliente.findOne({
-      where: {
-        email,
-      },
-    });
-
-    if (!cliente) {
-      return res.status(400).json("email não cadastrado!");
-    }
-
-    if (!bcrypty.compare(cliente.senha, senha )) {
-      return res
-        .status(401)
-        .json("E-mail ou senha inválido, verifique e tente novamente!");
-    }
-
-    const token = jwt.sign(
-      {
-        id: cliente._id,
-       },
-      secret.key
+    try {
+      const { email, senha } = req.body;
       
-    );
+      const cliente = await Cliente.findOne({
+          email,
+      });
 
-    
+      console.log(cliente)
 
-    // const token = jwt.sign({
-    //   id: cliente.id,
-    // }, process.env.JWT_PASS ?? '', {expiresIn: "8h"} );
+      if (!cliente) {
+        return res.status(400).json("email não cadastrado!");
+      }
 
-    // console.log(token);
+      if (!bcrypty.compareSync(senha, cliente.senha )) {
+        
+          return res
+          .status(401)
+          .json("E-mail ou senha inválido, verifique e tente novamente!");
+         
+      }
 
-    return res.json({error: false, message: 'Cliente logado com sucesso!', token});
+      const token = jwt.sign(
+        {
+          id: cliente._id,
+          email: cliente.email,
+          senha: cliente.senha
+        },
+        secret.key
+      );
+
+      return res.json({
+        error: false,
+        message: "Cliente logado com sucesso!",
+        token,
+      });
+    } catch (error) {
+      console.log(error)
+    }
   },
 };
 
